7c531c59c32e47add968be01ef591c96b6c5edf3
Samak95 <nouletsylvain@hotmail.fr>
2015-03-10 12:24:04 +0100

    ajout bdd

---
+++ SNCF/Metier/utilisateur.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNCF.Metier
+{
+    public class Utilisateur
+    {
+        public int id {get; set;}
+        public String nom { get; set;}
+        public String prenom { get; set; }
+        public String mail { get; set; }
+        public String login { get; set; }
+        public String mdp { get; set; }
+
+        public Utilisateur(int id, string nom, string prenom, string mail, string login, string mdp)
+        {
+            this.id = id;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.mail = mail;
+            this.login = login;
+            this.mdp = mdp;
+        }
+
+        public Utilisateur()
+        {
+
+        }
+    }
+}
---
+++ SNCF/readme.txt
@@ -0,0 +1,8 @@
+﻿CREATE TABLE utilisateur (id int(10) AUTO_INCREMENT PRIMARY KEY,
+nom varchar(255),
+prenom varchar(255),
+login varchar(255) unique not null,
+mdp varchar(255) not null,
+mail varchar(255) unique not null);
+
+http://blog.mounier-logiciels.fr/comment-creer-une-connection-mysql-local-en-CSharp.html
